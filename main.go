899ed1277cca2b410ed88208d67561b0d74b101a
Nimsaja <jasmin.roeper@nexgo.de>
2018-09-17 20:10:54 +0200

    change to parallel execution

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/Nimsaja/DepotPerformance/depot"
@@ -19,16 +20,22 @@ func main() {
 
 	euro, _ = getClose(depot.Stock{Symbol: "USD-EUR", Count: 1})
 
-	var v float32
-	var d string
+	//to check if every go routine is done
+	wg := sync.WaitGroup{}
+	//need to tell the wait group how many go routines we have
+	wg.Add(len(depot.Get()))
 	for _, s := range depot.Get() {
-		v, d = getClose(s)
-		fmt.Printf("Value for %v on %v is %v Euro\n", s.Name, d, v*euro)
+		go func(s depot.Stock) {
+			//will be called after this func is done, no matter where
+			defer wg.Done()
+			v, d := getClose(s)
+			fmt.Printf("Value for %v on %v is %v Euro\n", s.Name, d, v*euro)
+		}(s)
 	}
 
-	end := time.Now()
-	elapsedTime := end.Sub(start)
-	fmt.Println("Elapsed Time ", elapsedTime)
+	//here we wait for all the go routines to be done
+	wg.Wait()
+	fmt.Println("Elapsed Time ", time.Now().Sub(start))
 }
 
 func getClose(s depot.Stock) (float32, string) {
