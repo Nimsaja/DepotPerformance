9025c9f18d3e43305080b2ab78df41a0610a95c6
Nimsaja <jasmin.roeper@nexgo.de>
2018-09-25 17:14:40 +0200

    decode json object from yahoo page

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,15 +10,17 @@ import (
 	"time"
 
 	"github.com/Nimsaja/DepotPerformance/depot"
+	"github.com/Nimsaja/DepotPerformance/yahoo"
 )
 
 var url = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols="
 var euro float32
 
 func main() {
-	start := time.Now()
+	quotesYesterday := make([]float32, 0)
+	quotesToday := make([]float32, 0)
 
-	// euro, _ = getQuote(depot.Stock{Symbol: "USD-EUR", Count: 1})
+	start := time.Now()
 
 	//to check for raceconditions -> go run -race main.go
 	//to check if every go routine is done
@@ -29,39 +32,64 @@ func main() {
 			//will be called after this func is done, no matter where
 			defer wg.Done()
 			v := getQuote(s)
-			fmt.Printf("Value for %v is %v\n", s.Name, v)
+			fmt.Printf("Result for %v is %v\n", s.Name, v)
+
+			quotesYesterday = append(quotesYesterday, v.Close*s.Count)
+			quotesToday = append(quotesToday, v.Price*s.Count)
 		}(s)
 	}
 
 	//here we wait for all the go routines to be done
 	wg.Wait()
 	fmt.Println("Elapsed Time ", time.Now().Sub(start))
+
+	fmt.Println("Depot yesterday ", sum(quotesYesterday), " / Depot today ", sum(quotesToday))
+	fmt.Println("-> Diff ", sum(quotesToday)-sum(quotesYesterday))
 }
 
-func getQuote(s depot.Stock) (value float32) {
+func sum(input []float32) float32 {
+	sum := float32(0.0)
+
+	for i := range input {
+		sum += input[i]
+	}
+
+	return sum
+}
+
+func getQuote(s depot.Stock) (result yahoo.Result) {
+	res := yahoo.Result{}
+
 	u := fmt.Sprintf(url+"%v", s.Symbol)
 
 	resp, err := http.Get(u)
 	if err != nil {
 		log.Printf("Error %v ", err)
-		return 0
+		return res
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Printf("Error reading body: %v", err)
-		return 0
+		return res
 	}
 
-	n := len(body)
-	txt := string(body[:n])
+	// n := len(body)
+	// txt := string(body[:n])
 
-	fmt.Println("Result for ", s.Name, "is ", txt)
+	// fmt.Println("Result for ", s.Name, "is ", txt)
 
-	// result := json.NewDecoder(body)
+	out := yahoo.QuoteResponse{}
+	err = json.Unmarshal(body, &out)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 
-	// fmt.Println("Result for ", s.Name, "is ", result)
+	if len(out.QR.Res) > 0 {
+		res = out.QR.Res[0]
+	}
 
-	return 107
+	return res
 }
---
+++ yahoo/output.go
@@ -0,0 +1,21 @@
+package yahoo
+
+// QuoteResponse ...
+type QuoteResponse struct {
+	QR Output `json:"quoteResponse"`
+}
+
+// Output ...
+type Output struct {
+	Res   []Result    `json:"result"`
+	Error interface{} `json:"error"`
+}
+
+// Result ...
+type Result struct {
+	Cur    string  `json:"currency"`
+	Name   string  `json:"longName"`
+	Close  float32 `json:"regularMarketPreviousClose"`
+	Price  float32 `json:"regularMarketPrice"`
+	Symbol string  `json:"symbol"`
+}
