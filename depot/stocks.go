993a9e402a5b49ea6f86926f58288e9b34a5e101
Nimsaja <jasmin.roeper@nexgo.de>
2018-09-28 10:50:45 +0200

    show diff values for all stocks - new prop Buy for stocks

--- depot/stocks.go
+++ depot/stocks.go
@@ -5,19 +5,20 @@ type Stock struct {
 	Name   string
 	Symbol string
 	Count  float32
+	Buy    float32
 }
 
 //Get gets the portfolio
 func Get() []Stock {
 	return []Stock{
-		Stock{Name: "Google", Symbol: "ABEC.DE", Count: 0.211},
-		Stock{Name: "Amazon", Symbol: "AMZ.DE", Count: 0.056},
-		Stock{Name: "Netflix", Symbol: "NFC.DE", Count: 2},
-		Stock{Name: "Siemens", Symbol: "SIE.DE", Count: 5},
-		Stock{Name: "XING", Symbol: "O1BC.SG", Count: 2},
-		Stock{Name: "Biotech", Symbol: "DWWD.F", Count: 3},
-		Stock{Name: "Auto&Robotic", Symbol: "2B76.F", Count: 33},
-		Stock{Name: "TecDax", Symbol: "EXS2.F", Count: 10},
-		Stock{Name: "Oekoworld", Symbol: "OE7A.F", Count: 0.523},
+		Stock{Name: "Google", Symbol: "ABEC.DE", Count: 0.211, Buy: 1069.138},
+		Stock{Name: "Amazon", Symbol: "AMZ.DE", Count: 0.056, Buy: 1776.515},
+		Stock{Name: "Netflix", Symbol: "NFC.DE", Count: 2, Buy: 224.25},
+		Stock{Name: "Siemens", Symbol: "SIE.DE", Count: 5, Buy: 106.02},
+		Stock{Name: "XING", Symbol: "O1BC.SG", Count: 2, Buy: 328.765},
+		Stock{Name: "Biotech", Symbol: "DWWD.F", Count: 3, Buy: 195.693},
+		Stock{Name: "Auto&Robotic", Symbol: "2B76.F", Count: 33, Buy: 7.051},
+		Stock{Name: "TecDax", Symbol: "EXS2.F", Count: 10, Buy: 25.01},
+		Stock{Name: "Oekoworld", Symbol: "OE7A.F", Count: 0.523, Buy: 191.051},
 	}
 }
